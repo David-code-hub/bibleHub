be35e20b7f1de921df893635c868482fab245b16
David <macbookpro@Macbooks-MacBook-Pro.local>
2024-02-17 23:20:29 +0200

    add styling to signup

---
+++ layouts/default.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="relative isolate bg-white">
+    <div
+      class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      aria-hidden="true"
+    >
+      <div
+        class="bg-gradient-top"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
+    <header class="absolute inset-x-0 top-0 z-50">
+      <nav
+        class="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div class="flex lg:flex-1">
+          <NuxtLink to="/" class="-m-1.5 p-1.5">
+            <p class="font-bold text-2xl">BibleHub.</p>
+          </NuxtLink>
+        </div>
+        <div class="flex lg:hidden">
+          <button
+            type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = true"
+          >
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <NuxtLink to="/account/signup" class="btn">Sign Up</NuxtLink>
+          <NuxtLink to="/account/login" class="btn btn-bg-dark"
+            >Log In</NuxtLink
+          >
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        class="lg:hidden"
+        @close="mobileMenuOpen = false"
+        :open="mobileMenuOpen"
+      >
+        <div class="fixed inset-0 z-50" />
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <p class="font-bold mb-6">BibleHub</p>
+            </a>
+            <button
+              type="button"
+              class="-m-2.5 rounded-md p-2.5 text-gray-700"
+              @click="mobileMenuOpen = false"
+            >
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-0 flow-root">
+            <div class="mb-6 divide-y divide-gray-500/10">
+              <a href="#" class="btn grid my-3 grid-cols-4 w-3/4">Sign Up</a>
+              <a href="#" class="btn btn-bg-dark grid my-3 w-3/4">Log In</a>
+              <br />
+              <div class="pt-4">
+                <p
+                  class="-mx-3 px-3 py-2.5 italic text-sm font-normal leading-7 text-gray-700 hover:bg-gray-50"
+                >
+                  For God so loved the world, that he gave his only begotten
+                  Son, that whosoever believeth in him should not perish, but
+                  have everlasting life.
+                </p>
+                <p
+                  class="w-full text-right italic font-normal text-gray-600 text-sm"
+                >
+                  John 3:16
+                </p>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+    <slot />
+    <div
+      class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      aria-hidden="true"
+    >
+      <div
+        class="bg-gradient-bottom"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const mobileMenuOpen = ref(false);
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,112 +1,9 @@
 <template>
-  <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
-      <nav
-        class="flex items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
-            <p class="font-bold">BibleHub.</p>
-          </a>
-        </div>
-        <div class="flex lg:hidden">
-          <button
-            type="button"
-            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            @click="mobileMenuOpen = true"
-          >
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <NuxtLink to="/account/signup" class="btn">Sign Up</NuxtLink>
-          <NuxtLink to="/account/login" class="btn btn-bg-dark"
-            >Log In</NuxtLink
-          >
-        </div>
-      </nav>
-      <Dialog
-        as="div"
-        class="lg:hidden"
-        @close="mobileMenuOpen = false"
-        :open="mobileMenuOpen"
-      >
-        <div class="fixed inset-0 z-50" />
-        <DialogPanel
-          class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-        >
-          <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <p class="font-bold mb-6">BibleHub</p>
-            </a>
-            <button
-              type="button"
-              class="-m-2.5 rounded-md p-2.5 text-gray-700"
-              @click="mobileMenuOpen = false"
-            >
-              <span class="sr-only">Close menu</span>
-              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div class="mt-0 flow-root">
-            <div class="mb-6 divide-y divide-gray-500/10">
-              <a href="#" class="btn grid my-3 grid-cols-4 w-3/4">Sign Up</a>
-              <a href="#" class="btn btn-bg-dark grid my-3 w-3/4">Log In</a>
-              <br />
-              <div class="pt-4">
-                <p
-                  class="-mx-3 px-3 py-2.5 italic text-sm font-normal leading-7 text-gray-700 hover:bg-gray-50"
-                >
-                  For God so loved the world, that he gave his only begotten
-                  Son, that whosoever believeth in him should not perish, but
-                  have everlasting life.
-                </p>
-                <p
-                  class="w-full text-right italic font-normal text-gray-600 text-sm"
-                >
-                  John 3:16
-                </p>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
-
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div
-        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style="
-            clip-path: polygon(
-              74.1% 44.1%,
-              100% 61.6%,
-              97.5% 26.9%,
-              85.5% 0.1%,
-              80.7% 2%,
-              72.5% 32.5%,
-              60.2% 62.4%,
-              52.4% 68.1%,
-              47.5% 58.3%,
-              45.2% 34.5%,
-              27.5% 76.7%,
-              0.1% 64.9%,
-              17.9% 100%,
-              27.6% 76.8%,
-              76.1% 97.7%,
-              74.1% 44.1%
-            );
-          "
-        />
-      </div>
+  <div>
+    <div class="px-6 pt-14 lg:px-8">
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">
+          <h1 class="text-4xl font-bold tracking-tight sm:text-6xl text-black">
             BibleHub.
           </h1>
           <p class="mt-6 text-base leading-8 font-normal text-gray-600">
@@ -121,45 +18,17 @@
           </p>
           <p class="w-full font-normal text-sm">John 3:16</p>
           <div class="mt-7 flex items-center justify-center">
-            <NuxtLink to="/" class="btn btn-bg-dark">Explore More</NuxtLink>
-            <NuxtLink to="/account/signup" class="btn btn-outlined"
-              >Create Your Account <span aria-hidden="true">→</span></NuxtLink
+            <NuxtLink to="/" class="btn btn-bg-dark py-4 px-6"
+              >Explore More</NuxtLink
+            >
+            <NuxtLink to="/account/signup" class="btn btn-outlined py-4 px-6"
+              >Create Your Account</NuxtLink
             >
           </div>
         </div>
-      </div>
-      <div
-        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style="
-            clip-path: polygon(
-              74.1% 44.1%,
-              100% 61.6%,
-              97.5% 26.9%,
-              85.5% 0.1%,
-              80.7% 2%,
-              72.5% 32.5%,
-              60.2% 62.4%,
-              52.4% 68.1%,
-              47.5% 58.3%,
-              45.2% 34.5%,
-              27.5% 76.7%,
-              0.1% 64.9%,
-              17.9% 100%,
-              27.6% 76.8%,
-              76.1% 97.7%,
-              74.1% 44.1%
-            );
-          "
-        />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-const mobileMenuOpen = ref(false);
-</script>
+<script setup lang="ts"></script>
