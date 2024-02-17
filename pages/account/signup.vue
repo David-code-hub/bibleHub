229fb8a429a4adbd4e2893f0cab4b3b591f22e1f
David <macbookpro@Macbooks-MacBook-Pro.local>
2024-02-17 15:51:35 +0200

    add landing page styling

---
+++ pages/account/signup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid grid-rows-3 grid-flow-col gap-0 h-screen">
+    <div
+      class="row-span-3 col-span-2 bg-cover bg-[url('https://images.unsplash.com/photo-1527525443983-6e60c75fff46?q=80&w=1585&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
+    ></div>
+    <div class="row-span-3 col-span-1 bg-[#F0F3F9] p-4">
+      <p class="font-bold text-2xl">BibleHub.</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
